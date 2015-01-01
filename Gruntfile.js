a0e8881c48767c49e9bc0af0b298f2a77fc6b87f
William Petit <william.petit@ptitcloud.fr>
2015-01-01 14:43:05 +0100

    Ajout task installation automatique des dépendances bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,12 @@ var path = require('path');
 
 module.exports = function(grunt) {
 
-  var NW_VERSION = '0.11.3';
+  var NW_VERSION = '0.11.5';
   var BUILD_DIR = 'build';
   var BUILD_TARGETS = {
-    linux_ia32: true,
+    linux_ia32: false,
     linux_x64: true,
-    win: true,
+    win: false,
     osx: false
   };
   var PKG = grunt.file.readJSON('package.json');
@@ -27,42 +27,53 @@ module.exports = function(grunt) {
   // Define copy:build tasks files
   var snapFiles = [];
   _(BUILD_TARGETS).forEach(function(isEnabled, target) {
-    if(isEnabled) {
-      var arch = 'ia32';
-      var platform = target;
-      if(platform.indexOf('linux') !== -1) {
-        arch = platform.split('_')[1];
-        platform = 'linux';
-      }
-      var dirName = PKG.name + '-' + PKG.version + '-' + platform + '-' + arch;
-      var destPath = path.join(BUILD_DIR, dirName + '/');
 
-      // Add lib
-      snapFiles.push({src: 'lib/**', dest: destPath});
-
-      // Add themes
-      snapFiles.push({src: 'themes/**', dest: destPath});
-
-      // Add apps to build
-      snapFiles.push({src: 'apps/**', dest: destPath});
-
-      // Add snap dependencies
-      var modules = _.keys(PKG.dependencies).map(function(moduleName) {
-        return path.join('node_modules', moduleName, '**');
-      });
-      snapFiles.push({src: modules, dest: destPath});
-
-      // Add package.json
-      snapFiles.push({src: 'package.json', dest: destPath});
-
-      // Add main files, licence, & config
-      snapFiles.push({
-        src: ['index.html', 'app.js', 'config/defaults.yaml', 'LICENSE'],
-        dest: destPath}
-      );
-
+    if(!isEnabled) {
+      return;
     }
+
+    var arch = 'ia32';
+    var platform = target;
+    if(platform.indexOf('linux') !== -1) {
+      arch = platform.split('_')[1];
+      platform = 'linux';
+    }
+    var dirName = PKG.name + '-' + PKG.version + '-' + platform + '-' + arch;
+    var destPath = path.join(BUILD_DIR, dirName + '/');
+
+    // Add lib
+    snapFiles.push({src: 'lib/**', dest: destPath});
+
+    // Add themes
+    snapFiles.push({src: 'themes/**', dest: destPath});
+
+    // Add apps to build
+    snapFiles.push({src: 'apps/**', dest: destPath});
+
+    // Add snap dependencies
+    var modules = _.map(PKG.dependencies, function(version, moduleName) {
+      return path.join('node_modules', moduleName, '**');
+    });
+    snapFiles.push({src: modules, dest: destPath});
+
+    // Add package.json
+    snapFiles.push({src: 'package.json', dest: destPath});
+
+    // Add main files, licence, & config
+    snapFiles.push({
+      src: ['index.html', 'app.js', 'config/defaults.yaml', 'LICENSE'],
+      dest: destPath}
+    );
+
   });
+
+  // Apps with bower dependencies
+  var bowerApps = [
+    'apps/chat',
+    'apps/home',
+    'apps/user_profile',
+    'themes/default'
+  ];
 
   // Configure tasks
   grunt.initConfig({
@@ -101,6 +112,20 @@ module.exports = function(grunt) {
           }
         }
       }
+    },
+
+    shell: {
+      'bower-install': {
+        command: _.map(bowerApps, function(appPath) {
+          return 'cd "'+path.join(process.cwd(), appPath)+'"; bower install';
+        }).join('&&')
+      },
+      options: {
+        execOptions: {
+          cwd: process.cwd(),
+          failOnError: true
+        }
+      }
     }
 
   });
@@ -108,10 +133,11 @@ module.exports = function(grunt) {
   grunt.registerTask('snap:run',  ['download', 'run']);
   grunt.registerTask(
     'snap:build',
-    ['download', 'clean:build', 'build', 'copy:build']
+    ['download', 'shell:bower-install', 'clean:build', 'build', 'copy:build']
   );
   grunt.registerTask('default', ['snap:run']);
 
+  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-nw');
--- README.md
+++ README.md
@@ -8,31 +8,19 @@
 
 ## For developers & testers
 
-```
+```bash
 git clone https://github.com/Bornholm/snap.git
 cd snap
+# Download default apps
 git clone https://github.com/Bornholm/snap-apps.git apps
-sudo npm install grunt-cli -g
+# Install development tools
+sudo npm install grunt-cli bower bunyan -g
+# Install SNAP! dependencies
 npm install
-cd themes/default
-bower install
-cd ../..
-grunt
-```
-**'Headless' mode**
-
-You can launch SNAP! as a regular node application
-```
-cd snap
-node app
-```
-
-## Linux: Lack of libudev.so.0 ?
-
-See [node-webkit documentation](https://github.com/rogerwang/node-webkit/wiki/The-solution-of-lacking-libudev.so.0)
-
-**Workaround**
-```
-. tasks/build-res/linux/libudev-linker.sh
-grunt run
+# Install default apps & theme dependencies
+grunt shell:bower-install
+# Run in node-webkit
+grunt | bunyan
+# Or run as NodeJS app
+node app | bunyan
 ```
